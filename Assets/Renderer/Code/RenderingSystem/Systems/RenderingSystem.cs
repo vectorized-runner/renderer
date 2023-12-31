2d13b5c53fefb2a7206e40bdb85f81ec597f122c
Bartu Baş <bartubas99@gmail.com>
2023-12-31 11:48:58 +0300

    First Draw call prototype

---
+++ Assets/Renderer/Code/RenderingSystem/Systems/RenderingSystem.cs
@@ -0,0 +1,30 @@
+using Unity.Entities;
+using UnityEngine;
+
+namespace Renderer
+{
+    public class RenderData : IComponentData
+    {
+        public Camera Camera;
+    }
+
+
+    // TODO: Handle object layers later
+    [UpdateInGroup(typeof(PresentationSystemGroup))]
+    public partial class RenderingSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            var camera = Camera.main;
+
+            // Let's run DrawMesh first, non-burst
+            Entities.ForEach((in WorldMatrix worldMatrix, in RenderMeshIndex renderMeshIndex) =>
+            {
+                var renderMesh = RenderMeshRegisterSystem.GetRenderMesh(renderMeshIndex);
+
+                Graphics.DrawMesh(renderMesh.Mesh, worldMatrix.Value, renderMesh.Material, -1, camera,
+                    renderMesh.SubMeshIndex);
+            }).WithoutBurst().Run();
+        }
+    }
+}
