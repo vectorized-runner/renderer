70b839f5f59073411c975816091057c7b5cbc5e1
Bartu Baş <bartubas99@gmail.com>
2023-12-31 10:08:40 +0300

    RenderMesh setup

--- Assets/Renderer/Code/RenderingSystem/Components/RenderMesh.cs
+++ Assets/Renderer/Code/RenderingSystem/Components/RenderMesh.cs
@@ -1,11 +1,41 @@
+using System;
 using UnityEngine;
 
 namespace Renderer
 {
-    public struct RenderMesh
+    public readonly struct RenderMesh : IEquatable<RenderMesh>
     {
-        public Mesh Mesh;
-        public Material Material;
-        public int SubMeshIndex;
+        public readonly Mesh Mesh;
+        public readonly Material Material;
+        public readonly int SubMeshIndex;
+
+        public RenderMesh(Mesh mesh, Material material, int subMeshIndex)
+        {
+            Mesh = mesh;
+            Material = material;
+            SubMeshIndex = subMeshIndex;
+        }
+
+        public bool Equals(RenderMesh other)
+        {
+            return Mesh == other.Mesh && Material == other.Material && SubMeshIndex == other.SubMeshIndex;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is RenderMesh other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hash = 17;
+                hash = hash * 31 + Mesh.GetHashCode();
+                hash = hash * 31 + Material.GetHashCode();
+                hash = hash * 31 + SubMeshIndex.GetHashCode();
+                return hash;
+            }
+        }
     }
 }
