2ab9f7ddd30ba5dc58d2575b03355a37161942f0
Bartu Baş <bartubas99@gmail.com>
2024-01-03 23:40:46 +0300

    Handle Rendering on the UI side

---
+++ Assets/Renderer/Code/RenderStats/Systems/RenderStatsUI.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Renderer
+{
+    public class RenderStatsUI : MonoBehaviour
+    {
+        public TextMeshProUGUI AverageMsText;
+        public TextMeshProUGUI AverageFpsText;
+        
+        private void LateUpdate()
+        {
+            var world = World.DefaultGameObjectInjectionWorld;
+            var renderStatsSystem = world.GetExistingSystemManaged<RenderStatsSystem>();
+            var entityManager = world.EntityManager;
+            var renderStats = entityManager.GetComponentData<RenderStats>(renderStatsSystem.SystemHandle);
+
+            AverageMsText.text = $"{renderStats.AverageMs:n2} ms";
+            AverageFpsText.text = $"FPS: {renderStats.AverageFps:n2}";
+        }
+    }
+}
