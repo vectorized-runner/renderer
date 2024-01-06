80c822345bbf7fc00468638859fbefd4efa0fb85
Bartu Baş <bartubas99@gmail.com>
2024-01-06 19:20:01 +0300

    Attach chunk components

---
+++ Assets/Renderer/Code/Culling/Systems/AttachChunkComponentsSystem.cs
@@ -0,0 +1,22 @@
+using Unity.Entities;
+
+namespace Renderer
+{
+	public partial class AttachChunkComponentsSystem : SystemBase
+	{
+		private EntityQuery _chunkLackQuery;
+
+		protected override void OnCreate()
+		{
+			_chunkLackQuery = GetEntityQuery(
+				ComponentType.ChunkComponentExclude<ChunkWorldRenderBounds>(),
+				ComponentType.ChunkComponentExclude<ChunkCullResult>());
+		}
+
+		protected override void OnUpdate()
+		{
+			EntityManager.AddChunkComponentData(_chunkLackQuery, new ChunkWorldRenderBounds());
+			EntityManager.AddChunkComponentData(_chunkLackQuery, new ChunkCullResult());
+		}
+	}
+}
