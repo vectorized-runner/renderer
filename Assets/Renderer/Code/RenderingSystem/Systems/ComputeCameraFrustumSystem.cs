508d6057a7092178dfed9bf9ebddfa33265c2902
Bartu Baş <bartubas99@gmail.com>
2024-01-06 13:52:33 +0300

    Add: Camera Frustum Planes system

---
+++ Assets/Renderer/Code/RenderingSystem/Systems/ComputeCameraFrustumSystem.cs
@@ -0,0 +1,53 @@
+using System;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Renderer
+{
+    public partial class CalculateCameraFrustumPlanesSystem : SystemBase
+    {
+        // TODO: Include this NativeArray on Entity data (Read/Write checks)
+        public NativeArray<Plane> NativeFrustumPlanes;
+
+        private Camera _camera;
+        private Plane[] _frustumPlanes;
+
+        protected override void OnCreate()
+        {
+            _camera = Camera.main;
+            if (_camera == null)
+                _camera = Object.FindObjectOfType<Camera>();
+            if (_camera == null)
+                throw new Exception("Couldn't find any camera!");
+
+
+            _frustumPlanes = new Plane[6];
+            NativeFrustumPlanes = new NativeArray<Plane>(6, Allocator.Persistent);
+        }
+
+        protected override void OnDestroy()
+        {
+            NativeFrustumPlanes.Dispose();
+        }
+
+        protected override void OnUpdate()
+        {
+            GeometryUtility.CalculateFrustumPlanes(_camera, _frustumPlanes);
+            NativeFrustumPlanes.CopyFrom(_frustumPlanes);
+        }
+
+        public void DebugDrawCameraFrustum()
+        {
+            var corners = new Vector3[4];
+            _camera.CalculateFrustumCorners(new Rect(0, 0, 1, 1), _camera.farClipPlane, Camera.MonoOrStereoscopicEye.Mono,
+                corners);
+            for (int i = 0; i < corners.Length; i++)
+            {
+                var worldSpaceCorner = _camera.transform.TransformVector(corners[i]);
+                Debug.DrawRay(_camera.transform.position, worldSpaceCorner, Color.blue);
+            }
+        }
+    }
+}
