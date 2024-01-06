7f0e2a10e4e78f989313afe64e26f277b28faa94
Bartu Baş <bartubas99@gmail.com>
2024-01-06 13:35:50 +0300

    Add ComputeWorldRenderBoundsSystem

---
+++ Assets/Renderer/Code/RenderingSystem/Systems/ComputeWorldRenderBoundsSystem.cs
@@ -0,0 +1,64 @@
+using System.Runtime.CompilerServices;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Renderer
+{
+    public partial class ComputeWorldRenderBoundsSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            // TODO: Instead of LocalToWorld, use Translation, Rotation, Scale change filter
+            Entities
+                .WithNone<Static>()
+                .WithChangeFilter<LocalToWorld>()
+                .ForEach((ref WorldRenderBounds worldRenderBounds,
+                    in RenderBounds renderBounds,
+                    in LocalToWorld localToWorld) =>
+                {
+                    worldRenderBounds = CalculateWorldBounds(renderBounds, localToWorld);
+                })
+                .ScheduleParallel();
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static WorldRenderBounds CalculateWorldBounds(RenderBounds localBounds, LocalToWorld localToWorld)
+        {
+            var aabb = localBounds.AABB;
+            // Scaled orientation (?)
+            var localRight = localToWorld.Right * aabb.Extents.x;
+            var localUp = localToWorld.Up * aabb.Extents.y;
+            var localForward = localToWorld.Forward * aabb.Extents.z;
+            var right = math.right();
+            var up = math.up();
+            var forward = math.forward();
+
+            var newIi =
+                math.abs(math.dot(right, localRight)) +
+                math.abs(math.dot(right, localUp)) +
+                math.abs(math.dot(right, localForward));
+
+            var newIj =
+                math.abs(math.dot(up, localRight)) +
+                math.abs(math.dot(up, localUp)) +
+                math.abs(math.dot(up, localForward));
+
+            var newIk =
+                math.abs(math.dot(forward, localRight)) +
+                math.abs(math.dot(forward, localUp)) +
+                math.abs(math.dot(forward, localForward));
+
+            var worldExtents = new float3(newIi, newIj, newIk);
+            var center = localToWorld.Position;
+
+            return new WorldRenderBounds
+            {
+                AABB = new AABB
+                {
+                    Center = center,
+                    Extents = worldExtents
+                }
+            };
+        }
+    }
+}
