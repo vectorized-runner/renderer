7a1f0ecd725b3f9d897cab0a051d07de7d0daf74
Bartu Baş <bartubas99@gmail.com>
2024-01-06 18:38:36 +0300

    Fix-up the ChunkCullingJob

--- Assets/Renderer/Code/RenderingSystem/Systems/CullingSystem.cs
+++ Assets/Renderer/Code/RenderingSystem/Systems/CullingSystem.cs
@@ -1,7 +1,6 @@
 using Unity.Entities;
 using System.Collections.Generic;
 using Unity.Burst;
-using Unity.Burst.Intrinsics;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
 using Unity.Jobs;
@@ -11,65 +10,6 @@ using UnityEngine;
 
 namespace Renderer
 {
-    // How does IJobChunk work
-    // Think about a single Job per chunk
-    // Could be running on a separate thread
-    // What do we have on this Chunk -> an unique archetype, RenderMeshIndex we're writing to is single
-    // But multiple threads (Jobs) could be using the same RenderMeshIndex at the same time (need parallel write)
-    [BurstCompile]
-    public struct ChunkCullingJob : IJobChunk
-    {
-        [ReadOnly] public ComponentTypeHandle<RenderMeshIndex> RenderMeshHandle;
-        [ReadOnly] public ComponentTypeHandle<LocalToWorld> LocalToWorldHandle;
-        [ReadOnly] public ComponentTypeHandle<WorldRenderBounds> WorldRenderBoundsHandle;
-        [ReadOnly] public ComponentTypeHandle<ChunkWorldRenderBounds> ChunkWorldRenderBoundsHandle;
-        [ReadOnly] public NativeArray<Plane> FrustumPlanes;
-
-        public NativeArray<UnsafeStream> MatricesByRenderMeshIndex;
-
-        [NativeSetThreadIndex] public int ThreadIndex;
-
-        private void AssertValid(AABB aabb)
-        {
-            Debug.Assert(!math.any(math.isnan(aabb.Min)));
-            Debug.Assert(!math.any(math.isinf(aabb.Min)));
-            Debug.Assert(!math.any(math.isnan(aabb.Max)));
-            Debug.Assert(!math.any(math.isinf(aabb.Max)));
-        }
-
-        // Chunk A R=1, Chunk B R=1, Chunk C R=2, Thread 1, Thread 2
-
-        // Goal at this Job: Do check for culling, if culling passes, add entity matrix to render list
-        public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask,
-            in v128 chunkEnabledMask)
-        {
-            // TODO: Add culling later. Currently Just add all Entities for Rendering.
-            // TODO: Rethink the culling logic here (partial, full-in, full-out)
-            // TODO: If ChunkRenderBounds isn't visible, don't check the Entities for visibility
-
-            // All entities have different RenderMeshIndex value
-            var renderMeshIndexArray = chunk.GetNativeArray(ref RenderMeshHandle);
-            var worldRenderBoundsArray = chunk.GetNativeArray(ref WorldRenderBoundsHandle);
-            var localToWorldArray = chunk.GetNativeArray(ref LocalToWorldHandle);
-
-            var enumerator = new ChunkEntityEnumerator(useEnabledMask, chunkEnabledMask, chunk.Count);
-            while (enumerator.NextEntityIndex(out var i))
-            {
-                var aabb = worldRenderBoundsArray[i].AABB;
-
-                if (RMath.IsVisibleByCameraFrustum(FrustumPlanes, aabb))
-                {
-                    // We have the Entity right now. We want to add it for rendering.
-                    // But the Render Batches are by render mesh index
-                    // TODO: Is this thread-safe?
-                    var renderMeshIndex = renderMeshIndexArray[i].Value;
-                    var matrixStream = MatricesByRenderMeshIndex[renderMeshIndex];
-                    var matrixWriter = matrixStream.AsWriter();
-                }
-            }
-        }
-    }
-
     [BurstCompile]
     public unsafe struct ConvertStreamDataToArrayJob : IJobParallelFor
     {
