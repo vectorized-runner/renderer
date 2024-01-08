88d25b5633ebde34331f1a4347548622a16b121b
Bartu Baş <bartubas99@gmail.com>
2024-01-08 08:12:39 +0300

    WIP: Adding chunk intersection

--- Assets/Renderer/Code/Culling/Systems/ChunkCullingJob.cs
+++ Assets/Renderer/Code/Culling/Systems/ChunkCullingJob.cs
@@ -1,10 +1,8 @@
-using System.Runtime.CompilerServices;
+using System;
 using Unity.Burst;
 using Unity.Burst.Intrinsics;
 using Unity.Collections;
 using Unity.Entities;
-using Unity.Mathematics;
-using UnityEngine;
 using FrustumPlanes = Renderer.UnityPackages.FrustumPlanes;
 
 namespace Renderer
@@ -18,40 +16,58 @@ namespace Renderer
 
 		public ComponentTypeHandle<ChunkCullResult> ChunkCullResultHandle;
 
-		private void AssertValid(AABB aabb)
-		{
-			Debug.Assert(!math.any(math.isnan(aabb.Min)));
-			Debug.Assert(!math.any(math.isinf(aabb.Min)));
-			Debug.Assert(!math.any(math.isnan(aabb.Max)));
-			Debug.Assert(!math.any(math.isinf(aabb.Max)));
-		}
-
 		public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask,
 			in v128 chunkEnabledMask)
 		{
-			// TODO: Add culling later. Currently Just add all Entities for Rendering.
-			// TODO: If ChunkRenderBounds isn't visible, don't check the Entities for visibility
+			var chunkWorldRenderBounds = chunk.GetChunkComponentData(ref ChunkWorldRenderBoundsHandle);
+			var chunkAabb = chunkWorldRenderBounds.AABB;
+			var chunkIntersection = FrustumPlanes.Intersect2(PlanePackets, chunkAabb);
 
-			// All entities have different RenderMeshIndex value
-			var worldRenderBoundsArray = chunk.GetNativeArray(ref WorldRenderBoundsHandle);
-			var cullResult = new ChunkCullResult();
-
-			var enumerator = new ChunkEntityEnumerator(useEnabledMask, chunkEnabledMask, chunk.Count);
-			while (enumerator.NextEntityIndex(out var entityIndex))
+			switch (chunkIntersection)
 			{
-				var aabb = worldRenderBoundsArray[entityIndex].AABB;
-				var intersectResult = FrustumPlanes.Intersect2(PlanePackets, aabb);
-				var isVisible = intersectResult != FrustumPlanes.IntersectResult.Out;
-				// TODO: Remove Lower/Upper branch here. Could inline ChunkEntityEnumerator here
-				var lower = entityIndex < 64;
+				case FrustumPlanes.IntersectResult.Out:
+				{
+					// No Entity is visible, don't need to check Entity AABB's.
+					var cullResult = new ChunkCullResult { Lower = new BitField64(0), Upper = new BitField64(0) };
+					chunk.SetChunkComponentData(ref ChunkCullResultHandle, cullResult);
+					break;
+				}
+				case FrustumPlanes.IntersectResult.In:
+				{
+					// All Entities are visible, no need to check Entity AABB's.
+					// TODO:
 
-				if (lower)
-					cullResult.Lower.SetBits(entityIndex, isVisible);
-				else
-					cullResult.Upper.SetBits(64 - entityIndex, isVisible);
+					break;
+				}
+				case FrustumPlanes.IntersectResult.Partial:
+				{
+					// Check Each Entity individually
+					{
+						var worldRenderBoundsArray = chunk.GetNativeArray(ref WorldRenderBoundsHandle);
+						var cullResult = new ChunkCullResult();
+
+						var enumerator = new ChunkEntityEnumerator(useEnabledMask, chunkEnabledMask, chunk.Count);
+						while (enumerator.NextEntityIndex(out var entityIndex))
+						{
+							var aabb = worldRenderBoundsArray[entityIndex].AABB;
+							var intersectResult = FrustumPlanes.Intersect2(PlanePackets, aabb);
+							var isVisible = intersectResult != FrustumPlanes.IntersectResult.Out;
+							// TODO: Remove Lower/Upper branch here. Could inline ChunkEntityEnumerator here
+							var lower = entityIndex < 64;
+
+							if (lower)
+								cullResult.Lower.SetBits(entityIndex, isVisible);
+							else
+								cullResult.Upper.SetBits(64 - entityIndex, isVisible);
+						}
+
+						chunk.SetChunkComponentData(ref ChunkCullResultHandle, cullResult);
+					}
+					break;
+				}
+				default:
+					throw new ArgumentOutOfRangeException();
 			}
-			
-			chunk.SetChunkComponentData(ref ChunkCullResultHandle, cullResult);
 		}
 	}
 }
