7e49fc0276105d76586102bdf6a9e830de1f7547
Bartu Baş <bartubas99@gmail.com>
2024-02-09 21:04:43 +0300

    Finish NativeAtomicCounter implementation

--- Assets/Renderer/Code/Culling/Components/NativeAtomicCounter.cs
+++ Assets/Renderer/Code/Culling/Components/NativeAtomicCounter.cs
@@ -8,11 +8,56 @@ using Unity.Jobs.LowLevel.Unsafe;
 
 namespace Renderer
 {
+	/// <summary>
+	/// Implementation mostly taken from here: https://docs.unity3d.com/Packages/com.unity.jobs@0.2/manual/custom_job_types.html
+	/// Idea: Interlocked (Atomic) Add instruction is expensive, so use per-thread ints and accumulate,
+	/// but also always leave at least cache-line size between threads so they don't flush the cache (extra optimization)
+	/// </summary>
 	[StructLayout(LayoutKind.Sequential)]
 	[NativeContainer]
-	[NativeContainerIsAtomicWriteOnly]
 	public unsafe struct NativeAtomicCounter : IDisposable
 	{
+		[NativeContainer]
+		[NativeContainerIsAtomicWriteOnly]
+		public struct ParallelWriter
+		{
+			[NativeDisableUnsafePtrRestriction]
+			private int* _counterPerThread;
+
+			[NativeSetThreadIndex]
+			public int ThreadIndex;
+
+			// Copy of the AtomicSafetyHandle from the full NativeCounter.
+			// The dispose sentinel is not copied since this inner struct does not own the memory and is not responsible for freeing it.
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+			AtomicSafetyHandle m_Safety;
+#endif
+
+			// This is what makes it possible to assign to NativeCounter.Concurrent from NativeCounter
+			public static implicit operator ParallelWriter(NativeAtomicCounter cnt)
+			{
+				ParallelWriter parallelWriter;
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+				AtomicSafetyHandle.CheckWriteAndThrow(cnt.m_Safety);
+				parallelWriter.m_Safety = cnt.m_Safety;
+				AtomicSafetyHandle.UseSecondaryVersion(ref parallelWriter.m_Safety);
+#endif
+
+				parallelWriter._counterPerThread = cnt.CounterPerThread;
+				parallelWriter.ThreadIndex = 0;
+				return parallelWriter;
+			}
+
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			public void Increment(int amount = 1)
+			{
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+				AtomicSafetyHandle.CheckWriteAndThrow(m_Safety);
+#endif
+				_counterPerThread[ThreadIndex * IntsPerCacheLine] += amount;
+			}
+		}
+
 		[NoAlias]
 		[NativeDisableUnsafePtrRestriction]
 		public int* CounterPerThread;
@@ -20,17 +65,19 @@ namespace Renderer
 		public const int IntsPerCacheLine = JobsUtility.CacheLineSize / sizeof(int);
 		public const int ThreadCount = JobsUtility.MaxJobThreadCount;
 
-		[NativeSetThreadIndex] public int ThreadIndex;
+		[NativeSetThreadIndex]
+		public int ThreadIndex;
 
 		private Allocator _allocator;
 
 #if ENABLE_UNITY_COLLECTIONS_CHECKS
-		AtomicSafetyHandle m_Safety;
+		private AtomicSafetyHandle m_Safety;
 
 		// The dispose sentinel tracks memory leaks. It is a managed type so it is cleared to null when scheduling a job
 		// The job cannot dispose the container, and no one else can dispose it until the job has run, so it is ok to not pass it along
 		// This attribute is required, without it this NativeContainer cannot be passed to a job; since that would give the job access to a managed object
-		[NativeSetClassTypeToNullOnSchedule] DisposeSentinel m_DisposeSentinel;
+		[NativeSetClassTypeToNullOnSchedule]
+		private DisposeSentinel m_DisposeSentinel;
 #endif
 
 		public int Count
@@ -68,16 +115,6 @@ namespace Renderer
 
 				*CounterPerThread = value;
 			}
-		}
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public void Increment(int amount = 1)
-		{
-#if ENABLE_UNITY_COLLECTIONS_CHECKS
-			AtomicSafetyHandle.CheckWriteAndThrow(m_Safety);
-#endif
-
-			CounterPerThread[ThreadIndex * IntsPerCacheLine] += amount;
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
