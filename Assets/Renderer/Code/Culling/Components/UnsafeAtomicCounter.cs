fbbc485821bbb417c3ca06df65c68a3daa7b6b1d
Bartu Baş <bartubas99@gmail.com>
2024-02-10 01:18:01 +0300

    UnsafeAtomicCounter

---
+++ Assets/Renderer/Code/Culling/Components/UnsafeAtomicCounter.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Runtime.CompilerServices;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Jobs.LowLevel.Unsafe;
+
+namespace Renderer
+{
+	public unsafe struct UnsafeAtomicCounter : IDisposable
+	{
+		[NoAlias]
+		[NativeDisableUnsafePtrRestriction]
+		public int* CounterPerThread;
+
+		public Allocator Allocator;
+
+		public const int IntsPerCacheLine = JobsUtility.CacheLineSize / sizeof(int);
+		public const int ThreadCount = JobsUtility.MaxJobThreadCount;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Increment(int threadIndex, int amount)
+		{
+			CounterPerThread[threadIndex * IntsPerCacheLine] += amount;
+		}
+
+		public int Count
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get
+			{
+				var result = 0;
+
+				for (int i = 0; i < ThreadCount; i++)
+				{
+					result += CounterPerThread[i * IntsPerCacheLine];
+				}
+
+				return result;
+			}
+			set
+			{
+				for (int i = 1; i < JobsUtility.MaxJobThreadCount; ++i)
+				{
+					CounterPerThread[IntsPerCacheLine * i] = 0;
+				}
+
+				*CounterPerThread = value;
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public UnsafeAtomicCounter(Allocator allocator)
+		{
+			CounterPerThread = (int*)UnsafeUtility.Malloc(JobsUtility.CacheLineSize * ThreadCount,
+				JobsUtility.CacheLineSize, allocator);
+
+			Allocator = allocator;
+			Count = 0;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Dispose()
+		{
+			UnsafeUtility.Free(CounterPerThread, Allocator);
+			CounterPerThread = null;
+		}
+	}
+}
