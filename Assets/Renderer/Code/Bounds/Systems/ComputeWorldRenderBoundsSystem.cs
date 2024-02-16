f548d0c69ec6c21f45716860abd4512d363a6386
Bartu Baş <bartubas99@gmail.com>
2024-02-16 09:26:43 +0300

    Move method to static class

--- Assets/Renderer/Code/Bounds/Systems/ComputeWorldRenderBoundsSystem.cs
+++ Assets/Renderer/Code/Bounds/Systems/ComputeWorldRenderBoundsSystem.cs
@@ -1,6 +1,4 @@
-using System.Runtime.CompilerServices;
 using Unity.Entities;
-using Unity.Mathematics;
 
 namespace Renderer
 {
@@ -9,8 +7,6 @@ namespace Renderer
 	{
 		protected override void OnUpdate()
 		{
-			// TODO-Renderer: Instead of LocalToWorld, use Translation, Rotation, Scale change filter
-			// Static Entities don't have RenderBounds anyway, but still adding the filter.
 			Entities
 				.WithNone<Static>()
 				.WithChangeFilter<LocalToWorld>()
@@ -18,49 +14,9 @@ namespace Renderer
 					in RenderBounds renderBounds,
 					in LocalToWorld localToWorld) =>
 				{
-					worldRenderBounds = CalculateWorldBounds(renderBounds, localToWorld);
+					worldRenderBounds = RenderMath.CalculateWorldBounds(renderBounds, localToWorld);
 				})
 				.ScheduleParallel();
-		}
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static WorldRenderBounds CalculateWorldBounds(RenderBounds localBounds, LocalToWorld localToWorld)
-		{
-			var aabb = localBounds.AABB;
-			// Scaled orientation (?)
-			var localRight = localToWorld.Right * aabb.Extents.x;
-			var localUp = localToWorld.Up * aabb.Extents.y;
-			var localForward = localToWorld.Forward * aabb.Extents.z;
-			var right = math.right();
-			var up = math.up();
-			var forward = math.forward();
-
-			var newIi =
-				math.abs(math.dot(right, localRight)) +
-				math.abs(math.dot(right, localUp)) +
-				math.abs(math.dot(right, localForward));
-
-			var newIj =
-				math.abs(math.dot(up, localRight)) +
-				math.abs(math.dot(up, localUp)) +
-				math.abs(math.dot(up, localForward));
-
-			var newIk =
-				math.abs(math.dot(forward, localRight)) +
-				math.abs(math.dot(forward, localUp)) +
-				math.abs(math.dot(forward, localForward));
-
-			var worldExtents = new float3(newIi, newIj, newIk);
-			var center = localToWorld.Position;
-
-			return new WorldRenderBounds
-			{
-				AABB = new AABB
-				{
-					Center = center,
-					Extents = worldExtents
-				}
-			};
 		}
 	}
 }
--- Assets/Renderer/Code/Bounds/Systems/RenderMath.cs
+++ Assets/Renderer/Code/Bounds/Systems/RenderMath.cs
@@ -17,5 +17,46 @@ namespace Renderer
 				Extents = (newMax - newMin) * 0.5f
 			};
 		}
+		
+		
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static WorldRenderBounds CalculateWorldBounds(RenderBounds localBounds, LocalToWorld localToWorld)
+		{
+			var aabb = localBounds.AABB;
+			// Scaled orientation (?)
+			var localRight = localToWorld.Right * aabb.Extents.x;
+			var localUp = localToWorld.Up * aabb.Extents.y;
+			var localForward = localToWorld.Forward * aabb.Extents.z;
+			var right = math.right();
+			var up = math.up();
+			var forward = math.forward();
+
+			var newIi =
+				math.abs(math.dot(right, localRight)) +
+				math.abs(math.dot(right, localUp)) +
+				math.abs(math.dot(right, localForward));
+
+			var newIj =
+				math.abs(math.dot(up, localRight)) +
+				math.abs(math.dot(up, localUp)) +
+				math.abs(math.dot(up, localForward));
+
+			var newIk =
+				math.abs(math.dot(forward, localRight)) +
+				math.abs(math.dot(forward, localUp)) +
+				math.abs(math.dot(forward, localForward));
+
+			var worldExtents = new float3(newIi, newIj, newIk);
+			var center = localToWorld.Position;
+
+			return new WorldRenderBounds
+			{
+				AABB = new AABB
+				{
+					Center = center,
+					Extents = worldExtents
+				}
+			};
+		}
 	}
 }
