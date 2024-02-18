4e8f7264e6d0469fc7c15e06760749abd8ae8d8f
Bartu Baş <bartubas99@gmail.com>
2024-02-18 14:40:22 +0300

    AABB.ToString

--- Assets/Renderer/Code/Bounds/Components/AABB.cs
+++ Assets/Renderer/Code/Bounds/Components/AABB.cs
@@ -10,12 +10,9 @@ namespace Renderer
 		public float3 Min => Center - Extents;
 		public float3 Max => Center + Extents;
 
-		public void Encapsulate(float3 point)
+		public override string ToString()
 		{
-			var newMin = math.min(Min, point);
-			var newMax = math.max(Max, point);
-			Center = (newMin + newMax) * 0.5f;
-			Extents = (newMax - newMin) * 0.5f;
+			return $"Center: {Center}, Extents: {Extents}";
 		}
 	}
 }
