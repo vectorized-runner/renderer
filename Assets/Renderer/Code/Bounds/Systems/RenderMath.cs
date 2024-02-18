0c1e87f34fd3e9e13c2587beec1615d1584ff62d
Bartu Baş <bartubas99@gmail.com>
2024-02-18 13:49:34 +0300

    Remove unused EncapsulateAABBs

--- Assets/Renderer/Code/Bounds/Systems/RenderMath.cs
+++ Assets/Renderer/Code/Bounds/Systems/RenderMath.cs
@@ -8,20 +8,6 @@ namespace Renderer
 	public static class RenderMath
 	{
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static AABB EncapsuleAABBs(AABB first, AABB second)
-		{
-			var newMin = math.min(first.Min, second.Min);
-			var newMax = math.max(first.Max, second.Max);
-
-			return new AABB
-			{
-				Center = (newMin + newMax) * 0.5f,
-				Extents = (newMax - newMin) * 0.5f
-			};
-		}
-
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static WorldRenderBounds ComputeWorldRenderBounds(RenderBounds localBounds, LocalToWorld localToWorld)
 		{
 			var aabb = localBounds.AABB;
