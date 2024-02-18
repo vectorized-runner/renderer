d4317c05fdfedf414b416232d0d72782d53c0f42
Bartu Baş <bartubas99@gmail.com>
2024-02-18 10:08:51 +0300

    AABB.Encapsulate

--- Assets/Renderer/Code/Bounds/Components/AABB.cs
+++ Assets/Renderer/Code/Bounds/Components/AABB.cs
@@ -9,5 +9,13 @@ namespace Renderer
 
 		public float3 Min => Center - Extents;
 		public float3 Max => Center + Extents;
+
+		public void Encapsulate(float3 point)
+		{
+			var newMin = math.min(Min, point);
+			var newMax = math.max(Max, point);
+			Center = (newMin + newMax) * 0.5f;
+			Extents = (newMax - newMin) * 0.5f;
+		}
 	}
 }
