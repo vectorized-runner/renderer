9710acf54d177fc8e1fed7de18f6d901e554fe98
Bartu Baş <bartubas99@gmail.com>
2024-02-18 12:46:58 +0300

    Element Access checks

--- Assets/Renderer/Code/Util/CollectionUtil.cs
+++ Assets/Renderer/Code/Util/CollectionUtil.cs
@@ -1,14 +1,36 @@
 using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
-using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace Renderer
 {
 	public static unsafe class CollectionUtil
 	{
+		[Conditional("UNITY_EDITOR")]
+		[Conditional("DEVELOPMENT_BUILD")]
+		[Conditional("ENABLE_UNITY_COLLECTIONS_CHECKS")]
+		private static void CheckElementAccess(int index, int count)
+		{
+			if (index < 0 || index >= count)
+				throw new InvalidOperationException($"Access index '{index}' is out of range of count '{count}'");
+		}
+
+		[Conditional("UNITY_EDITOR")]
+		[Conditional("DEVELOPMENT_BUILD")]
+		[Conditional("ENABLE_UNITY_COLLECTIONS_CHECKS")]
+		private static void CheckElementAccessRange(int index, int length, int count)
+		{
+			if (length < 0)
+				throw new InvalidOperationException($"Invalid length '{length}'");
+
+			CheckElementAccess(index, count);
+			CheckElementAccess(index + length, count);
+		}
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static T* GetTypedPtr<T>(this NativeArray<T> array) where T : unmanaged
 		{
@@ -24,74 +46,84 @@ namespace Renderer
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static ref T ElementAsRef<T>(this NativeList<T> list, int index) where T : unmanaged
 		{
+			CheckElementAccess(index, list.Length);
 			return ref UnsafeUtility.ArrayElementAsRef<T>(list.GetUnsafePtr(), index);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static ref readonly T ElementAsReadonlyRef<T>(this NativeList<T> list, int index) where T : unmanaged
 		{
+			CheckElementAccess(index, list.Length);
 			return ref UnsafeUtility.ArrayElementAsRef<T>(list.GetUnsafeReadOnlyPtr(), index);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static ref T ElementAsRef<T>(this NativeArray<T> array, int index) where T : unmanaged
 		{
+			CheckElementAccess(index, array.Length);
 			return ref UnsafeUtility.ArrayElementAsRef<T>(array.GetUnsafePtr(), index);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static ref readonly T ElementAsReadonlyRef<T>(this NativeArray<T> array, int index) where T : unmanaged
 		{
+			CheckElementAccess(index, array.Length);
 			return ref UnsafeUtility.ArrayElementAsRef<T>(array.GetUnsafeReadOnlyPtr(), index);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static Span<T> AsSpan<T>(this UnsafeList<T> list, int startIndex, int length) where T : unmanaged
 		{
-			return GetSpan(list.Ptr, startIndex, length);
+			CheckElementAccessRange(startIndex, length, list.Length);
+			return GetUnsafeSpan(list.Ptr, startIndex, length);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static ReadOnlySpan<T> AsReadOnlySpan<T>(this UnsafeList<T> list, int startIndex, int length)
 			where T : unmanaged
 		{
-			return GetReadOnlySpan(list.Ptr, startIndex, length);
+			CheckElementAccessRange(startIndex, length, list.Length);
+			return GetUnsafeReadOnlySpan(list.Ptr, startIndex, length);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static Span<T> AsSpan<T>(this NativeList<T> list, int startIndex, int length) where T : unmanaged
 		{
-			return GetSpan(list.GetTypedPtr(), startIndex, length);
+			CheckElementAccessRange(startIndex, length, list.Length);
+			return GetUnsafeSpan(list.GetTypedPtr(), startIndex, length);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static ReadOnlySpan<T> AsReadOnlySpan<T>(this NativeList<T> list, int startIndex, int length)
 			where T : unmanaged
 		{
-			return GetReadOnlySpan(list.GetTypedPtr(), startIndex, length);
+			CheckElementAccessRange(startIndex, length, list.Length);
+			return GetUnsafeReadOnlySpan(list.GetTypedPtr(), startIndex, length);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static Span<T> AsSpan<T>(this NativeArray<T> array, int startIndex, int length) where T : unmanaged
 		{
-			return GetSpan(array.GetTypedPtr(), startIndex, length);
+			CheckElementAccessRange(startIndex, length, array.Length);
+			return GetUnsafeSpan(array.GetTypedPtr(), startIndex, length);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static ReadOnlySpan<T> AsReadOnlySpan<T>(this NativeArray<T> array, int startIndex, int length)
 			where T : unmanaged
 		{
-			return GetReadOnlySpan(array.GetTypedPtr(), startIndex, length);
+			CheckElementAccessRange(startIndex, length, array.Length);
+			return GetUnsafeReadOnlySpan(array.GetTypedPtr(), startIndex, length);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static ReadOnlySpan<T> GetReadOnlySpan<T>(T* ptr, int startIndex, int length) where T : unmanaged
+		public static ReadOnlySpan<T> GetUnsafeReadOnlySpan<T>(T* ptr, int startIndex, int length) where T : unmanaged
 		{
 			return new ReadOnlySpan<T>(ptr + startIndex, length);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Span<T> GetSpan<T>(T* ptr, int startIndex, int length) where T : unmanaged
+		public static Span<T> GetUnsafeSpan<T>(T* ptr, int startIndex, int length) where T : unmanaged
 		{
 			return new Span<T>(ptr + startIndex, length);
 		}
