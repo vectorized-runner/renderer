e0883f9c0db40c17c7ed678428faaab057483350
Bartu Baş <bartubas99@gmail.com>
2024-02-20 12:10:04 +0300

    Remove boilerplate code

--- Assets/Renderer/Code/Debug/Systems/AABBDrawHelper.cs
+++ Assets/Renderer/Code/Debug/Systems/AABBDrawHelper.cs
@@ -14,31 +14,13 @@ namespace Renderer
 			Debug.Log("AAAAAA");
 
 			var aabbSystem = World.DefaultGameObjectInjectionWorld.GetExistingSystemManaged<AABBDebugDrawSystem>();
-
-			int lineCount = 100;
-			float radius = 3.0f;
-
 			aabbSystem._lineMaterial.SetPass(0);
 
 			GL.PushMatrix();
-			// Set transformation matrix for drawing to
-			// match our transform
 			GL.MultMatrix(transform.localToWorldMatrix);
-
-			// Draw lines
 			GL.Begin(GL.LINES);
-			for (int i = 0; i < lineCount; ++i)
 			{
-				float a = i / (float)lineCount;
-				float angle = a * Mathf.PI * 2;
-				// Vertex colors change from red to green
-				GL.Color(new Color(a, 1 - a, 0, 0.8F));
-				// One vertex at transform position
-				GL.Vertex3(0, 0, 0);
-				// Another vertex at edge of circle
-				GL.Vertex3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius, 0);
 			}
-
 			GL.End();
 			GL.PopMatrix();
 		}
