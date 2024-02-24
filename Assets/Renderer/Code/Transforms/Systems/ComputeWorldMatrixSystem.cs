12be3752c5971af3693f30d8fae01e54b332b6d1
Bartu Baş <bartubas99@gmail.com>
2024-02-24 15:45:49 +0300

    WIP: Handling objects with Parent Component

--- Assets/Renderer/Code/Transforms/Systems/ComputeWorldMatrixSystem.cs
+++ Assets/Renderer/Code/Transforms/Systems/ComputeWorldMatrixSystem.cs
@@ -9,13 +9,15 @@ namespace Renderer
 		protected override void OnUpdate()
 		{
 			Entities
-				.WithNone<Static>()
+				.WithNone<Static, Parent>()
 				.ForEach(
 					(ref LocalToWorld worldMatrix, in Position position, in Rotation rotation, in Scale scale) =>
 					{
 						worldMatrix.Value = float4x4.TRS(position.Value, rotation.Value, scale.Value);
 					})
 				.ScheduleParallel();
+			
+			// TODO: Handle objects with Parent component
 		}
 	}
 }
