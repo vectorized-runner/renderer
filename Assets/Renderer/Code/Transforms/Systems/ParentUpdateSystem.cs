25bfbc81557df9a35454dc556e8ad80c8737c22a
Bartu Baş <bartubas99@gmail.com>
2024-02-29 13:44:53 +0300

    Initial Logic: Removing Parent component works

--- Assets/Renderer/Code/Transforms/Systems/ParentUpdateSystem.cs
+++ Assets/Renderer/Code/Transforms/Systems/ParentUpdateSystem.cs
@@ -20,20 +20,25 @@ namespace Renderer
 			var parentByRemovedChildren = new NativeParallelMultiHashMap<Entity, Entity>(64, Allocator.TempJob);
 
 			// If Parent is removed from a Child Entity, that Child Entity should get removed from the Parent's Child List
-			var dep1 = new CollectRemovedChildrenJob
+			var dep1 = new HandleChildrenWithRemovedParentJob
 			{
 				EntityHandle = GetEntityTypeHandle(),
 				PreviousParentHandle = GetComponentTypeHandle<PreviousParent>(true),
 				ParentByRemovedChildren = parentByRemovedChildren.AsParallelWriter(),
+				LocalToWorldHandle = GetComponentTypeHandle<LocalToWorld>(),
+				LocalToWorldLookup = GetComponentLookup<LocalToWorld>(true),
+				LocalTransformHandle = GetComponentTypeHandle<LocalTransform>(),
 			}.ScheduleParallel(_removedParentsQuery, Dependency);
-
+			
 			var dep2 = new RemoveChildrenJob
 			{
 				ChildLookup = GetBufferLookup<Child>(),
 				ParentRemovedChildrenMap = parentByRemovedChildren
 			}.Schedule(dep1);
-
+			
 			Dependency = parentByRemovedChildren.Dispose(dep2);
+
+			EntityManager.RemoveComponent<PreviousParent>(_removedParentsQuery);
 		}
 	}
 }
