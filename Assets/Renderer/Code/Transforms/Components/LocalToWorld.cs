9edc4b1a6f956720025764a53ac35dfcd7608a67
Bartu Baş <bartubas99@gmail.com>
2024-02-29 13:30:14 +0300

    Add: LocalToWorld.Rotation

--- Assets/Renderer/Code/Transforms/Components/LocalToWorld.cs
+++ Assets/Renderer/Code/Transforms/Components/LocalToWorld.cs
@@ -13,6 +13,9 @@ namespace Renderer
 		public float3 Up => Value.c1.xyz;
 		public float3 Forward => Value.c2.xyz;
 		public float3 Position => Value.c3.xyz;
+
+		public quaternion Rotation => new quaternion(math.orthonormalize(new float3x3(Value)));
+
 		public float3 ScaleXYZ =>
 			new float3(math.length(Value.c0.xyz), math.length(Value.c1.xyz), math.length(Value.c2.xyz));
 
