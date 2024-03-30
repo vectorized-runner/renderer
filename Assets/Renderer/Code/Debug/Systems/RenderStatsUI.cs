e28d87bf58761f5a1b7dc0e028caf3098e4f605b
Bartu Baş <bartubas99@gmail.com>
2024-03-30 09:57:03 +0300

    Debug UI cleanup

--- Assets/Renderer/Code/Debug/Systems/RenderStatsUI.cs
+++ Assets/Renderer/Code/Debug/Systems/RenderStatsUI.cs
@@ -40,17 +40,17 @@ namespace Renderer
 			var culledPercentage = (int)math.floor(100 * (float)renderStats.CulledCount / renderStats.TotalObjectCount);
 			sb.AppendLine($"{renderStats.AverageMs:n2} ms");
 			sb.AppendLine($"FPS: {renderStats.AverageFps}");
-			sb.AppendLine($"Total Objects: {renderStats.TotalObjectCount:N0}");
-			sb.AppendLine($"Render Objects: {renderStats.RenderedCount:N0}");
-			sb.AppendLine($"Culled Objects: {renderStats.CulledCount:N0}");
+			sb.AppendLine($"Total Object Count: {renderStats.TotalObjectCount:N0}");
+			sb.AppendLine($"Render Object Count: {renderStats.RenderedCount:N0}");
+			sb.AppendLine($"Culled Object Count: {renderStats.CulledCount:N0}");
 			sb.AppendLine($"Culled Percentage: {culledPercentage}%");
 			sb.AppendLine($"Render Verts: {renderStats.RenderVertsCount:N0}");
 			sb.AppendLine($"Render Tris: {renderStats.RenderTrisCount:N0}");
-			sb.AppendLine($"Render Batches: {renderStats.RenderBatchCount:N0}");
-			sb.AppendLine($"ECS Chunks: {renderStats.ChunkCount:N0}");
-			sb.AppendLine($"Chunk Out: {renderStats.OutChunks:N0}");
-			sb.AppendLine($"Chunk In: {renderStats.InChunks:N0}");
-			sb.AppendLine($"Chunk Partial: {renderStats.PartialChunks:N0}");
+			sb.AppendLine($"Render Batch Count: {renderStats.RenderBatchCount:N0}");
+			sb.AppendLine($"Chunk Count: {renderStats.ChunkCount:N0}");
+			sb.AppendLine($"Chunk Frustum Out: {renderStats.OutChunks:N0}");
+			sb.AppendLine($"Chunk Frustum In: {renderStats.InChunks:N0}");
+			sb.AppendLine($"Chunk Frustum Partial: {renderStats.PartialChunks:N0}");
 			sb.AppendLine($"Memory Used (MB): {renderStats.MemoryUsedInMb:0.00}");
 			sb.AppendLine($"Unique Mesh Count: {renderStats.UniqueMeshCount:N0}");
 
