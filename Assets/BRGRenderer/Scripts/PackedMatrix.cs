90978ae28bd37ed9f89134fc300e15c8ec44f4cf
Bartu Baş <bartubas99@gmail.com>
2025-04-01 15:27:30 +0300

    Add PackedMatrix data type

---
+++ Assets/BRGRenderer/Scripts/PackedMatrix.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace BRGRenderer
+{
+    // The PackedMatrix is a convenience type that converts matrices into
+    // the format that Unity-provided SRP shaders expect.
+    // (48 bytes instead of 64 bytes - saves GPU bandwidth)
+    public struct PackedMatrix
+    {
+        public float c0x;
+        public float c0y;
+        public float c0z;
+        public float c1x;
+        public float c1y;
+        public float c1z;
+        public float c2x;
+        public float c2y;
+        public float c2z;
+        public float c3x;
+        public float c3y;
+        public float c3z;
+
+        public PackedMatrix(Matrix4x4 m)
+        {
+            c0x = m.m00;
+            c0y = m.m10;
+            c0z = m.m20;
+            c1x = m.m01;
+            c1y = m.m11;
+            c1z = m.m21;
+            c2x = m.m02;
+            c2y = m.m12;
+            c2z = m.m22;
+            c3x = m.m03;
+            c3y = m.m13;
+            c3z = m.m23;
+        }
+    }
+}
