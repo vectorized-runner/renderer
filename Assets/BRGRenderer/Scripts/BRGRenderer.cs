20579485d45a7f346b74f50cf60f1a544dcf70a9
Bartu Baş <bartubas99@gmail.com>
2025-04-01 16:32:48 +0300

    Add code. Crashed

--- Assets/BRGRenderer/Scripts/BRGRenderer.cs
+++ Assets/BRGRenderer/Scripts/BRGRenderer.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Jobs;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -147,9 +149,71 @@ namespace BRGRenderer
             BatchCullingOutput cullingOutput,
             IntPtr userContext)
         {
-            // This example doesn't use jobs, so it can return an empty JobHandle.
-            // Performance-sensitive applications should use Burst jobs to implement
-            // culling and draw command output. In this case, this function would return a
+            // UnsafeUtility.Malloc() requires an alignment, so use the largest integer type's alignment
+            // which is a reasonable default.
+            int alignment = UnsafeUtility.AlignOf<long>();
+
+            // Acquire a pointer to the BatchCullingOutputDrawCommands struct so you can easily
+            // modify it directly.
+            var drawCommands = (BatchCullingOutputDrawCommands*)cullingOutput.drawCommands.GetUnsafePtr();
+
+            // Allocate memory for the output arrays. In a more complicated implementation, you would calculate
+            // the amount of memory to allocate dynamically based on what is visible.
+            // This example assumes that all of the instances are visible and thus allocates
+            // memory for each of them. The necessary allocations are as follows:
+            // - a single draw command (which draws kNumInstances instances)
+            // - a single draw range (which covers our single draw command)
+            // - kNumInstances visible instance indices.
+            // You must always allocate the arrays using Allocator.TempJob.
+            drawCommands->drawCommands =
+                (BatchDrawCommand*)UnsafeUtility.Malloc(UnsafeUtility.SizeOf<BatchDrawCommand>(), alignment,
+                    Allocator.TempJob);
+            drawCommands->drawRanges = (BatchDrawRange*)UnsafeUtility.Malloc(UnsafeUtility.SizeOf<BatchDrawRange>(),
+                alignment, Allocator.TempJob);
+            drawCommands->visibleInstances =
+                (int*)UnsafeUtility.Malloc(_instanceCount * sizeof(int), alignment, Allocator.TempJob);
+            drawCommands->drawCommandPickingInstanceIDs = null;
+
+            drawCommands->drawCommandCount = 1;
+            drawCommands->drawRangeCount = 1;
+            drawCommands->visibleInstanceCount = _instanceCount;
+
+            // This example doens't use depth sorting, so it leaves instanceSortingPositions as null.
+            drawCommands->instanceSortingPositions = null;
+            drawCommands->instanceSortingPositionFloatCount = 0;
+
+            // Configure the single draw command to draw kNumInstances instances
+            // starting from offset 0 in the array, using the batch, material and mesh
+            // IDs registered in the Start() method. It doesn't set any special flags.
+            drawCommands->drawCommands[0].visibleOffset = 0;
+            drawCommands->drawCommands[0].visibleCount = _instanceCount;
+            drawCommands->drawCommands[0].batchID = _batchID;
+            drawCommands->drawCommands[0].materialID = _materialIdByMaterial.First().Value;
+            drawCommands->drawCommands[0].meshID = _meshIdByMesh.First().Value;
+            drawCommands->drawCommands[0].submeshIndex = 0;
+            drawCommands->drawCommands[0].splitVisibilityMask = 0xff;
+            drawCommands->drawCommands[0].flags = 0;
+            drawCommands->drawCommands[0].sortingPosition = 0;
+
+            // Configure the single draw range to cover the single draw command which
+            // is at offset 0.
+            drawCommands->drawRanges[0].drawCommandsBegin = 0;
+            drawCommands->drawRanges[0].drawCommandsCount = 1;
+
+            // This example doesn't care about shadows or motion vectors, so it leaves everything
+            // at the default zero values, except the renderingLayerMask which it sets to all ones
+            // so Unity renders the instances regardless of mask settings.
+            drawCommands->drawRanges[0].filterSettings = new BatchFilterSettings { renderingLayerMask = 0xffffffff, };
+
+            // Finally, write the actual visible instance indices to the array. In a more complicated
+            // implementation, this output would depend on what is visible, but this example
+            // assumes that everything is visible.
+            for (int i = 0; i < _instanceCount; ++i)
+                drawCommands->visibleInstances[i] = i;
+
+            // This simple example doesn't use jobs, so it returns an empty JobHandle.
+            // Performance-sensitive applications are encouraged to use Burst jobs to implement
+            // culling and draw command output. In this case, this function returns a
             // handle here that completes when the Burst jobs finish.
             return new JobHandle();
         }
