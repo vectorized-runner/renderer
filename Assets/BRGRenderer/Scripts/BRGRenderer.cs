8ec0c873a7a51a9db5b145da55a390273524f0ab
Bartu Baş <bartubas99@gmail.com>
2025-04-02 06:23:18 +0300

    Rename fields

--- Assets/BRGRenderer/Scripts/BRGRenderer.cs
+++ Assets/BRGRenderer/Scripts/BRGRenderer.cs
@@ -12,28 +12,25 @@ namespace BRGRenderer
         public Mesh mesh;
         public Material material;
 
-        private BatchRendererGroup m_BRG;
+        private BatchRendererGroup _brg;
 
-        private GraphicsBuffer m_InstanceData;
-        private BatchID m_BatchID;
-        private BatchMeshID m_MeshID;
-        private BatchMaterialID m_MaterialID;
+        private GraphicsBuffer _graphicsBuffer;
+        private BatchID _batchID;
+        private BatchMeshID _meshID;
+        private BatchMaterialID _materialID;
 
         // Some helper constants to make calculations more convenient.
-        private const int kSizeOfMatrix = sizeof(float) * 4 * 4;
-        private const int kSizeOfPackedMatrix = sizeof(float) * 4 * 3;
-        private const int kSizeOfFloat4 = sizeof(float) * 4;
-        private const int kBytesPerInstance = (kSizeOfPackedMatrix * 2) + kSizeOfFloat4;
-        private const int kExtraBytes = kSizeOfMatrix * 2;
-        private const int kNumInstances = 3;
-
-
-
+        private const int _unityMatrixSize = sizeof(float) * 4 * 4;
+        private const int _brgMatrixSize = sizeof(float) * 4 * 3;
+        private const int _float4Size = sizeof(float) * 4;
+        private const int _sizePerRenderObject = (_brgMatrixSize * 2) + _float4Size;
+        private const int _extraBytes = _unityMatrixSize * 2;
+        private const int _renderObjectCount = 3;
         private void Start()
         {
-            m_BRG = new BatchRendererGroup(this.OnPerformCulling, IntPtr.Zero);
-            m_MeshID = m_BRG.RegisterMesh(mesh);
-            m_MaterialID = m_BRG.RegisterMaterial(material);
+            _brg = new BatchRendererGroup(OnPerformCulling, IntPtr.Zero);
+            _meshID = _brg.RegisterMesh(mesh);
+            _materialID = _brg.RegisterMaterial(material);
 
             AllocateInstanceDateBuffer();
             PopulateInstanceDataBuffer();
@@ -41,8 +38,8 @@ namespace BRGRenderer
 
         private void AllocateInstanceDateBuffer()
         {
-            m_InstanceData = new GraphicsBuffer(GraphicsBuffer.Target.Raw,
-                BufferCountForInstances(kBytesPerInstance, kNumInstances, kExtraBytes),
+            _graphicsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Raw,
+                BufferCountForInstances(_sizePerRenderObject, _renderObjectCount, _extraBytes),
                 sizeof(int));
         }
 
@@ -52,7 +49,7 @@ namespace BRGRenderer
             var zero = new Matrix4x4[1] { Matrix4x4.zero };
 
             // Create transform matrices for three example instances.
-            var matrices = new Matrix4x4[kNumInstances]
+            var matrices = new Matrix4x4[_renderObjectCount]
             {
                 Matrix4x4.Translate(new Vector3(-2, 0, 0)),
                 Matrix4x4.Translate(new Vector3(0, 0, 0)),
@@ -60,7 +57,7 @@ namespace BRGRenderer
             };
 
             // Convert the transform matrices into the packed format that shaders expects.
-            var objectToWorld = new PackedMatrix[kNumInstances]
+            var objectToWorld = new PackedMatrix[_renderObjectCount]
             {
                 new PackedMatrix(matrices[0]),
                 new PackedMatrix(matrices[1]),
@@ -68,7 +65,7 @@ namespace BRGRenderer
             };
 
             // Also create packed inverse matrices.
-            var worldToObject = new PackedMatrix[kNumInstances]
+            var worldToObject = new PackedMatrix[_renderObjectCount]
             {
                 new PackedMatrix(matrices[0].inverse),
                 new PackedMatrix(matrices[1].inverse),
@@ -76,7 +73,7 @@ namespace BRGRenderer
             };
 
             // Make all instances have unique colors.
-            var colors = new Vector4[kNumInstances]
+            var colors = new Vector4[_renderObjectCount]
             {
                 new Vector4(1, 0, 0, 1),
                 new Vector4(0, 1, 0, 1),
@@ -95,15 +92,17 @@ namespace BRGRenderer
             // address 96 instead of 64 which means 32 bits are left uninitialized. This is because the 
             // computeBufferStartIndex parameter requires the start offset to be divisible by the size of the source
             // array element type. In this case, it's the size of PackedMatrix, which is 48.
-            uint byteAddressObjectToWorld = kSizeOfPackedMatrix * 2;
-            uint byteAddressWorldToObject = byteAddressObjectToWorld + kSizeOfPackedMatrix * kNumInstances;
-            uint byteAddressColor = byteAddressWorldToObject + kSizeOfPackedMatrix * kNumInstances;
+            uint byteAddressObjectToWorld = _brgMatrixSize * 2;
+            uint byteAddressWorldToObject = byteAddressObjectToWorld + _brgMatrixSize * _renderObjectCount;
+            uint byteAddressColor = byteAddressWorldToObject + _brgMatrixSize * _renderObjectCount;
 
             // Upload the instance data to the GraphicsBuffer so the shader can load them.
-            m_InstanceData.SetData(zero, 0, 0, 1);
-            m_InstanceData.SetData(objectToWorld, 0, (int)(byteAddressObjectToWorld / kSizeOfPackedMatrix), objectToWorld.Length);
-            m_InstanceData.SetData(worldToObject, 0, (int)(byteAddressWorldToObject / kSizeOfPackedMatrix), worldToObject.Length);
-            m_InstanceData.SetData(colors, 0, (int)(byteAddressColor / kSizeOfFloat4), colors.Length);
+            _graphicsBuffer.SetData(zero, 0, 0, 1);
+            _graphicsBuffer.SetData(objectToWorld, 0, (int)(byteAddressObjectToWorld / _brgMatrixSize),
+                objectToWorld.Length);
+            _graphicsBuffer.SetData(worldToObject, 0, (int)(byteAddressWorldToObject / _brgMatrixSize),
+                worldToObject.Length);
+            _graphicsBuffer.SetData(colors, 0, (int)(byteAddressColor / _float4Size), colors.Length);
 
             // Set up metadata values to point to the instance data. Set the most significant bit 0x80000000 in each
             // which instructs the shader that the data is an array with one value per instance, indexed by the instance index.
@@ -112,13 +111,16 @@ namespace BRGRenderer
             // 0x00000000 metadata value and loads from the start of the buffer. The start of the buffer which is
             // is a zero matrix so this sort of load is guaranteed to return zero, which is a reasonable default value.
             var metadata = new NativeArray<MetadataValue>(3, Allocator.Temp);
-            metadata[0] = new MetadataValue { NameID = Shader.PropertyToID("unity_ObjectToWorld"), Value = 0x80000000 | byteAddressObjectToWorld, };
-            metadata[1] = new MetadataValue { NameID = Shader.PropertyToID("unity_WorldToObject"), Value = 0x80000000 | byteAddressWorldToObject, };
-            metadata[2] = new MetadataValue { NameID = Shader.PropertyToID("_BaseColor"), Value = 0x80000000 | byteAddressColor, };
+            metadata[0] = new MetadataValue
+                { NameID = Shader.PropertyToID("unity_ObjectToWorld"), Value = 0x80000000 | byteAddressObjectToWorld, };
+            metadata[1] = new MetadataValue
+                { NameID = Shader.PropertyToID("unity_WorldToObject"), Value = 0x80000000 | byteAddressWorldToObject, };
+            metadata[2] = new MetadataValue
+                { NameID = Shader.PropertyToID("_BaseColor"), Value = 0x80000000 | byteAddressColor, };
 
             // Finally, create a batch for the instances, and make the batch use the GraphicsBuffer with the
             // instance data, as well as the metadata values that specify where the properties are. 
-            m_BatchID = m_BRG.AddBatch(metadata, m_InstanceData.bufferHandle);
+            _batchID = _brg.AddBatch(metadata, _graphicsBuffer.bufferHandle);
         }
 
         // Raw buffers are allocated in ints. This is a utility method that calculates
@@ -135,7 +137,7 @@ namespace BRGRenderer
 
         private void OnDisable()
         {
-            m_BRG.Dispose();
+            _brg.Dispose();
         }
 
         public unsafe JobHandle OnPerformCulling(
@@ -160,14 +162,18 @@ namespace BRGRenderer
             // - a single draw range (which covers our single draw command)
             // - kNumInstances visible instance indices.
             // You must always allocate the arrays using Allocator.TempJob.
-            drawCommands->drawCommands = (BatchDrawCommand*)UnsafeUtility.Malloc(UnsafeUtility.SizeOf<BatchDrawCommand>(), alignment, Allocator.TempJob);
-            drawCommands->drawRanges = (BatchDrawRange*)UnsafeUtility.Malloc(UnsafeUtility.SizeOf<BatchDrawRange>(), alignment, Allocator.TempJob);
-            drawCommands->visibleInstances = (int*)UnsafeUtility.Malloc(kNumInstances * sizeof(int), alignment, Allocator.TempJob);
+            drawCommands->drawCommands =
+                (BatchDrawCommand*)UnsafeUtility.Malloc(UnsafeUtility.SizeOf<BatchDrawCommand>(), alignment,
+                    Allocator.TempJob);
+            drawCommands->drawRanges = (BatchDrawRange*)UnsafeUtility.Malloc(UnsafeUtility.SizeOf<BatchDrawRange>(),
+                alignment, Allocator.TempJob);
+            drawCommands->visibleInstances =
+                (int*)UnsafeUtility.Malloc(_renderObjectCount * sizeof(int), alignment, Allocator.TempJob);
             drawCommands->drawCommandPickingInstanceIDs = null;
 
             drawCommands->drawCommandCount = 1;
             drawCommands->drawRangeCount = 1;
-            drawCommands->visibleInstanceCount = kNumInstances;
+            drawCommands->visibleInstanceCount = _renderObjectCount;
 
             // This example doens't use depth sorting, so it leaves instanceSortingPositions as null.
             drawCommands->instanceSortingPositions = null;
@@ -177,10 +183,10 @@ namespace BRGRenderer
             // starting from offset 0 in the array, using the batch, material and mesh
             // IDs registered in the Start() method. It doesn't set any special flags.
             drawCommands->drawCommands[0].visibleOffset = 0;
-            drawCommands->drawCommands[0].visibleCount = kNumInstances;
-            drawCommands->drawCommands[0].batchID = m_BatchID;
-            drawCommands->drawCommands[0].materialID = m_MaterialID;
-            drawCommands->drawCommands[0].meshID = m_MeshID;
+            drawCommands->drawCommands[0].visibleCount = _renderObjectCount;
+            drawCommands->drawCommands[0].batchID = _batchID;
+            drawCommands->drawCommands[0].materialID = _materialID;
+            drawCommands->drawCommands[0].meshID = _meshID;
             drawCommands->drawCommands[0].submeshIndex = 0;
             drawCommands->drawCommands[0].splitVisibilityMask = 0xff;
             drawCommands->drawCommands[0].flags = 0;
@@ -200,7 +206,7 @@ namespace BRGRenderer
             // Finally, write the actual visible instance indices to the array. In a more complicated
             // implementation, this output would depend on what is visible, but this example
             // assumes that everything is visible.
-            for (int i = 0; i < kNumInstances; ++i)
+            for (int i = 0; i < _renderObjectCount; ++i)
                 drawCommands->visibleInstances[i] = i;
 
             // This simple example doesn't use jobs, so it returns an empty JobHandle.
